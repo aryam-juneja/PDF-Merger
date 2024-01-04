9eead6c9c5f3366d1dfccef5961c58cbb0b810e3
Beelzebub <49526278+Aryam14@users.noreply.github.com>
2024-01-05 02:31:48 +0530

    added different functionalities

--- modules.py
+++ modules.py
@@ -2,43 +2,67 @@ import datetime
 import json
 import os
 
-pdfList = open('pdfList.txt', 'r+').read().splitlines()
+# pdfList = open('pdfList.txt', 'r+').read().splitlines()
 
 # get list of list file
 def get_list(filename='pdfList.txt'):
     return open(filename, 'r+').read().splitlines()
 
 # add file to list file
-def appdent_to_list(file):
+def append_to_list(file):
     with open('pdfList.txt', 'a+') as f:
         f.write(file + '\n')
         f.close()
 
-# history dump
+# history dumps
 def store_history(name, filename='history.json'):
-    with open(filename, 'r+') as file:
+        if os.path.isfile(filename) and os.path.getsize(filename) > 0:
+            with open(filename, 'r+') as file:
+                file_data = json.load(file)
+        else:
+            file_data = {'history': []}
 
-        file_data = json.load(file)
         file_data['history'].append({
             'name-merged': name,
-            'files-merged': get_list(),
-            'date': datetime.datetime.now()
+            'files-merged': get_filename(),
+            'date and time': datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
         })
         file.seek(0)
         json.dump(file_data, file, indent=4)
 
 # check if the file exists and is a pdf
 def search_selection(file):
-    if file in os.listdir and file.endswith(".pdf"):
+    dir = os.listdir()
+    if file in dir and file.endswith(".pdf"):
         return True
     return False
 
 #get filename only from the path
-def get_filename(file):
-    return file.split('/')[-1]
+def get_filename(file_list = get_list()):
+    names = []
+    
+    for file in file_list:
+        # file_list.replace(file, file.split('/')[-1])
+        names.append(file.split('/')[-1])
+        
+    return names
 
 #add extension if no extension is present
 def add_ext(file):
     if file.endswith(".pdf"):
         return file
     return file + '.pdf'
+
+#clear the list
+def clear_list(filename='pdfList.txt'):
+    open(filename, 'w').close()
+
+#remove file from the list
+def remove_selection(idx, filename='pdfList.txt'):
+    with open(filename, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            if line != get_list()[int(idx)-1]:
+                f.write(line)
+        f.truncate()
--- pdfMerger.py
+++ pdfMerger.py
@@ -1,40 +1,8 @@
 # import required libraries
 from PyPDF2 import PdfWriter
-import datetime
-import json
 import click
 import os
-
-pdfList = []
-
-# history dump
-def store_history(name, filename='history.json'):
-    with open(filename, 'r+') as file:
-
-        file_data = json.load(file)
-        file_data['history'].append({
-            'name-merged': name,
-            'files-merged': pdfList,
-            'date': datetime.datetime.now()
-        })
-        file.seek(0)
-        json.dump(file_data, file, indent=4)
-
-# check if the file exists and is a pdf
-def search_selection(file):
-    if file in os.listdir and file.endswith(".pdf"):
-        return True
-    return False
-
-#get filename only from the path
-def get_filename(file):
-    return file.split('/')[-1]
-
-#add extension if no extension is present
-def add_ext(file):
-    if file.endswith(".pdf"):
-        return file
-    return file + '.pdf'
+from modules import *
 
 @click.group()
 def commands():
@@ -42,41 +10,49 @@ def commands():
 
 # merge files
 @click.command()
-@click.argument('files', required=False, help='The name of the merged file')
-@click.argument('cat',)
-def merge(name, files, cat):
+@click.argument('files', required=False, nargs=-1, type=click.Path())
+@click.option('--cat', nargs=1, type=click.Path())
+def merge(files, cat):
     merger = PdfWriter()
-    if files is not None:
-        files.split(" ")
-        for file in files:
-            pdfList.append(file + '.pdf')
 
-    if len(pdfList) == 0 or 1:
-        print("No. of Files to merge:", len(pdfList), '\n')
+    #for file names provided as arguments
+    if files is not None:
+        for file in files:
+            append_to_list(os.path.join(os.getcwd(), add_ext(file)))
+
+    # throw exception
+    if len(get_list()) == 0 or len(get_list()) == 1:
+        print("No. of Files to merge:", len(get_list()), '\n')
         print("Cannot perform merge operation. Please add files to merge.")
         return
     
-    for pdf in pdfList:
+    # merging algo
+    for pdf in get_list():
         merger.append(pdf)
     
-    # mergedName = input("Enter the name of the merged file:") + '.pdf'
-    merger.write(name + '.pdf')
+    if cat is None:
+        cat = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    else: 
+        cat = click.prompt('Enter the name of the merged file', type=click.Path(), default=cat)
+    merger.write(add_ext(cat))
     merger.close()
-    store_history(name)
-    pdfList.clear()
+    store_history(cat)
+    clear_list()
 
 # add files
 @click.command()
-@click.argument('file', prompt='Enter the name of the file/s to merge:', help='The name of the file/s to merge')
-def add(file):
-    file.split(" ")
-    if(search_selection(file)):
-        pdfList.append(os.path.join(os.getcwd(), add_ext(file)))
+@click.argument('files', nargs=-1, type=click.Path())
+def add(files):
+    for file in files:
+        if(search_selection(add_ext(file))):
+            append_to_list(os.path.join(os.getcwd(), add_ext(file)))
+        else:
+            click.echo((click.style("File not found: ", fg='red')) + (click.style(file, fg='yellow')))
         
 @click.command()
 def list_sel():
     click.echo(click.style("List of the selected files:\n"))
-    for file in pdfList: 
+    for file in get_list(): 
         print(file)
 
 # list files in the open directory
@@ -86,12 +62,13 @@ def list_dir():
     i = 1
     for x in os.listdir():      # forms a list for all the files present in the directory
         if x.endswith(".pdf"):  # picks out the list members with '.pdf extention'
-            pdfList.append(x)
+            append_to_list(x)
             print(i,'. ',x)
             i += 1
 
+# to change the order of the files
 @click.command()
-@click.argument('order', prompt='Enter the order of the files to merge:', help='The order of the files to merge')
+@click.argument('order')
 def reorder(order):
     order.split(" ")
     if len(order) != len(pdfList):
@@ -105,15 +82,23 @@ def reorder(order):
 
 @click.command()
 def clear():
-    pdfList.clear()
+    # pdfList.clear()
     click.echo(click.style("Selcetion cleared successfully!"))
 
 @click.command()
-@click.argument('idx', prompt='Remove what laa?:', help='The name of the file to remove')
+@click.argument('idx')
 def remove(idx):
     idx.split(" ")
     for i in idx:
-        pdfList.pop(int(i)-1)
+        remove_selection(i-1)
+
+commands.add_command(merge)
+commands.add_command(add)
+commands.add_command(list_sel)
+commands.add_command(list_dir)
+commands.add_command(reorder)
+commands.add_command(clear)
+commands.add_command(remove)
 
 if __name__ == "__main__":
     commands()
